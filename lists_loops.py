5fc7c2cf93379921d4af82560d0680537ad56d42
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-26 14:09:58 -0700

    printed "Do It" and "For The Night" using slice on the songs list

--- lists_loops.py
+++ lists_loops.py
@@ -1,3 +1,9 @@
+#Task: Copy the above list songs into your list_loops.py file
 songs = ["ROCKSTAR", "Do It", "For The Night"]
+
+#Task: print out the first and last element of the songs list in your list_loops.py file
 print(songs[0])
 print(songs[2])
+
+#Task: print out "Do It"and "For The Night" using a list slice on the songs list in your list_loops.py file
+print(songs[1:3])
