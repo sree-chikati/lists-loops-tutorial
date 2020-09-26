1b03bb776285dd1fe8f44a05a6066fd72ccc4037
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-26 14:18:19 -0700

    add three songs to songs list

--- lists_loops.py
+++ lists_loops.py
@@ -11,3 +11,11 @@ print(songs[1:3])
 #Task: update the last element in the songs list with a new song of your choice in your list_loops.py file
 songs[2] = "Butterfly"
 print(songs)
+
+#Task: add three songs of your choice to your songs list in your list_loops.py file
+songs.append("Run") #adds an element to the end of the list
+print(songs)
+songs.extend(["Save Me"]) #adds a list to end of a list
+print(songs)
+songs.insert(4, "Spring Day") #adds element at specific index followed by item
+print(songs)
