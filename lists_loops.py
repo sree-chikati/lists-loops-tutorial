e48614fdfc7e5ec7a120588d4d6bc6a19376f61b
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-26 14:11:36 -0700

    updated last element in songs with a new song

--- lists_loops.py
+++ lists_loops.py
@@ -7,3 +7,7 @@ print(songs[2])
 
 #Task: print out "Do It"and "For The Night" using a list slice on the songs list in your list_loops.py file
 print(songs[1:3])
+
+#Task: update the last element in the songs list with a new song of your choice in your list_loops.py file
+songs[2] = "Butterfly"
+print(songs)
